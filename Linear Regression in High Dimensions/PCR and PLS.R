cdd229168d88b69fbfd0aab14dcb35213d7f80d0
S Kancharla <31664851+Shilpa-Kancharla@users.noreply.github.com>
2018-05-04 22:17:13 -0400

    Add files via upload

--- Linear Regression in High Dimensions/PCR and PLS.R
+++ Linear Regression in High Dimensions/PCR and PLS.R
@@ -8,17 +8,39 @@ This is an [R Markdown](http://rmarkdown.rstudio.com) Notebook. When you execute
 Try executing this chunk by clicking the *Run* button within the chunk or by placing your cursor inside it and pressing *Cmd+Shift+Enter*. 
 
 ```{r}
-xtr <- matrix(rnorm(100*100), ncol = 100)
-beta <- c(rep(1,10), rep(0,90))
-ytr <- xtr%*%beta + rnorm(100)
+#PCR
 library(pls)
-out <- pcr(ytr~xtr, scale = TRUE, validation = "CV")
-summary(out)
-validationplot(out, val.type = "MSEP")
-
-out2 <- plsr(ytr~xtr, scale = TRUE, validation = "CV")
-summary(out)
-validationplot(out, val.type = "MSEP")
+set.seed(2)
+pcr.fit=pcr(Salary~., data=Hitters, scale=TRUE, validation="CV") #setting scale to TRUE allows for standardization. setting validation to CV computes a ten-fold cross validation error for each possible value of M, the number of principal components used.
+summary(pcr.fit)
+```
+```{r}
+validationplot(pcr.fit, val.type="MSEP")
+```
+```{r}
+#performing PCR ON THE Training data and evaluating its test set performance
+set.seed(1)
+pcr.fit=pcr(Salary~., data=Hitters, subset=train, scale=TRUE, validation="CV")
+validationplot(pcr.fit, val.type = "MSEP")
+```
+```{r}
+pcr.pred=predict(pcr.fit, x[test,], ncomp=7)
+mean((pcr.pred-y.test)^2)
+pcr.fit=pcr(y~x, scale=TRUE, ncomp=7)
+summary(pcr.fit)
+```
+```{r}
+#PLS
+set.seed(1)
+pls.fit=plsr(Salary~., data=Hitters, subset=train, scale=TRUE, validation="CV")
+summary(pls.fit)
+validationplot(pls.fit, val.type = "MSEP")
+```
+```{r}
+pls.pred=predict(pls.fit, x[test,], ncomp=2)
+mean((pls.pred-y.test)^2)
+pls.fit=plsr(Salary~., data=Hitters, scale=TRUE, ncomp=2)
+summary(pls.fit)
 ```
 
 Add a new chunk by clicking the *Insert Chunk* button on the toolbar or by pressing *Cmd+Option+I*.
