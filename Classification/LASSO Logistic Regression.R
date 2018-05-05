cb21a05b404fb88d33b42278d77f0993883f080e
S Kancharla <31664851+Shilpa-Kancharla@users.noreply.github.com>
2018-05-05 00:20:44 -0400

    Add files via upload

---
+++ Classification/LASSO Logistic Regression.R
@@ -0,0 +1,23 @@
+---
+title: "R Notebook"
+output: html_notebook
+---
+
+This is an [R Markdown](http://rmarkdown.rstudio.com) Notebook. When you execute code within the notebook, the results appear beneath the code. 
+
+Try executing this chunk by clicking the *Run* button within the chunk or by placing your cursor inside it and pressing *Cmd+Shift+Enter*. 
+
+```{r}
+xtr <- matrix(rnorm(1000*20), ncol = 20)
+beta <- c(rep(1,5), rep(0,15))
+ytr <- 1*((xtr%*%beta + .5*rnorm(1000)) >= 0)
+cv.out <- cv.glmnet(xtr, ytr, family = "binomial", alpha = 1)
+plot(cv.out)
+```
+
+Add a new chunk by clicking the *Insert Chunk* button on the toolbar or by pressing *Cmd+Option+I*.
+
+When you save the notebook, an HTML file containing the code and output will be saved alongside it (click the *Preview* button or press *Cmd+Shift+K* to preview the HTML file). 
+
+The preview shows you a rendered HTML copy of the contents of the editor. Consequently, unlike *Knit*, *Preview* does not run any R code chunks. Instead, the output of the chunk when it was last run in the editor is displayed.
+
