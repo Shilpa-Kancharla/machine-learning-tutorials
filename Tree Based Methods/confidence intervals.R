fba9eb83562a36938cff14ef725ccbb06cd8a7ca
S Kancharla <31664851+Shilpa-Kancharla@users.noreply.github.com>
2018-05-06 13:56:53 -0400

    Create confidence intervals.R

---
+++ Tree Based Methods/confidence intervals.R
@@ -0,0 +1,14 @@
+> t.test(x)
+
+# One sample t-test
+
+data <- x
+t = 5.7446
+df = 9
+p-value = 0.0002782
+alternative hypothesis: turn mean is not equal to 0
+95 percent confidence interval: 
+  3.334139  7.665851
+sample estimates:
+mean of x
+    5.5
